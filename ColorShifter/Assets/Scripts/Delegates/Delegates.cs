e29e4303e924ef5eff0a999ba3a6547d10f2f5af
Dicky Chouryan <dchouryan26@gmail.com>
2020-08-11 14:33:27 -0400

    Create a delegate and HComponent script

--- ColorShifter/Assets/Scripts/Delegates/Delegates.cs
+++ ColorShifter/Assets/Scripts/Delegates/Delegates.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ Delegate is function array
+ if Function matches type and parameters, it can be bound to delegate (EX: is of type void, and has no parameters () for this specific delegate)
+ Multicast means we're intending to bind multiple functions to the one delegate
+ We specify NoParams so we remember this delegate doesn't pass in data
+ */
+
+public delegate void MulticastNoParams();
+
+public delegate void MulticastOneParam(int value);
+public delegate void MulticastOneFloatParam(float value);
+
 public class Delegates : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
 }
--- ColorShifter/Assets/Scripts/Health/HealthComponent.cs
+++ ColorShifter/Assets/Scripts/Health/HealthComponent.cs
@@ -10,5 +10,59 @@ public class HealthComponent : MonoBehaviour
 
     [SerializeField] GameObject deathFX = null;
 
-    public
+    public MulticastNoParams OnDeath;
+    public MulticastOneParam OnDamaged;
+
+    void Awake()
+    {
+        maxHealth = health;
+    }
+
+    public virtual void ApplyDamage(int Damage)
+    {
+        //DAMAGE
+        if(Damage <= 0 || health <= 0)
+        {
+            return;
+        }
+
+        health -= Damage;
+
+        if(health <= 0)
+        {
+            Death();
+        }
+
+        if (OnDamaged != null)
+        {
+            //?. is a null reference check, it will not call the OnDamaged Delegate if there are no bound functions (meaning the delegate == null)
+            OnDamaged?.Invoke(Damage);
+        }
+        else
+        {
+            Debug.LogError(name + " No functions bound to OnDamaged delegate");
+        }
+    }
+
+    protected virtual void Death()
+    {
+        // DEATH
+        if(OnDeath != null)
+        {
+            OnDeath?.Invoke();
+        }
+        else
+        {
+            Debug.LogError(name + " No functions bound to OnDeath Delegate");
+        }
+
+        //PARTICLES (Currently no particles is made)
+        if(deathFX != null)
+        {
+                       //object      position          rotation. (transform.rotation if you want it to be auto)
+            Instantiate(deathFX, transform.position, Quaternion.Euler(-90, 0, 0));
+
+            Debug.Log("Health Component - Death Called");
+        }
+    }
 }
