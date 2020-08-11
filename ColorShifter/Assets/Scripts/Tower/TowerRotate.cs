eb4bd92d96b0327ff726f9b162d15e2c41583bd9
frank078 <46834129+frank078@users.noreply.github.com>
2020-08-11 13:05:37 -0600

    Made the tower rotation script and added it to the tower

---
+++ ColorShifter/Assets/Scripts/Tower/TowerRotate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class TowerRotate : MonoBehaviour
+{
+    //variables
+    Vector3 currentRotation;
+    public float newRotation = 45;
+
+    public float speed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Turn Right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentRotation = transform.eulerAngles;
+            currentRotation.y = Mathf.Lerp(currentRotation.y, currentRotation.y + newRotation, Time.deltaTime * speed);
+            transform.eulerAngles = currentRotation;
+        }
+
+        // Turn Left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentRotation = transform.eulerAngles;
+            currentRotation.y = Mathf.Lerp(currentRotation.y, currentRotation.y - newRotation, Time.deltaTime * speed);
+            transform.eulerAngles = currentRotation;
+        }
+    }
+}
