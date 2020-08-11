4ed82d82dc68fb994fb750a9a785d1d97ee97ef8
Dicky Chouryan <dchouryan26@gmail.com>
2020-08-11 14:02:26 -0400

    Create a subfolder for scripts

---
+++ ColorShifter/Assets/Scripts/Health/HealthComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public float health = 1;
+
+    float maxHealth;
+
+    [SerializeField] GameObject deathFX = null;
+
+    public
+}
