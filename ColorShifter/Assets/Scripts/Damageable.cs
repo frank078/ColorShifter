f66188d80bbbbaef6a8137a7275361146412830a
Dicky Chouryan <dchouryan26@gmail.com>
2020-08-11 14:59:19 -0400

    Player now can die
    Create a damageable script that damages the player

---
+++ ColorShifter/Assets/Scripts/Damageable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    // Update is called once per frame
+
+    public GameObject target;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameplayStatics.DealDamage(target, 1);
+        }
+    }
+}
---
+++ ColorShifter/Assets/Scripts/Statics/GameplayStatics.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// basically create a static class that will be access to everything.
+public static class GameplayStatics
+{
+    public static void DealDamage(GameObject DamagedObject, int Damage)
+    {
+        HealthComponent health = DamagedObject.GetComponent<HealthComponent>();
+        if (health)
+        {
+            health.ApplyDamage(Damage);
+        }
+    }
+}
