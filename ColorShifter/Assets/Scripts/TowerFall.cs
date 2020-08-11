e154fb429170b41ce592a6b5774621eeed4b74cb
frank078 <46834129+frank078@users.noreply.github.com>
2020-08-11 08:31:11 -0600

    Tower Fall
    Made a cylinder with physics to simulate the player going up a tower

---
+++ ColorShifter/Assets/Scripts/TowerFall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TowerFall : MonoBehaviour
+{
+    // Variables
+    public Rigidbody rb;
+    public float fallSpeed = -1000;
+
+    void FixedUpdate()
+    {
+        // This makes the tower fall towards the player
+        rb.AddForce(0, fallSpeed * Time.deltaTime, 0);
+    }
+}
