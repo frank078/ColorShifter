905e29c2a946fdce7bb8be589eab6cd7956d787d
Dicky Chouryan <dchouryan26@gmail.com>
2020-08-17 15:07:12 -0400

    Create an effects system

---
+++ ColorShifter/Assets/Scripts/Effects/Effects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effects : MonoBehaviour
+{
+    [SerializeField] float lifeTime = 3.0f;
+
+    [SerializeField] GameObject[] VFX; // that takes the particles
+
+    List<GameObject> spawnedEffects = new List<GameObject>(); // once a particle spawned, add it to here
+
+    void Start()
+    {
+        // foreach all gameobject in VFX and instantiate it and add it to the spawnedEffects list
+        foreach(GameObject effect in VFX)
+        {
+            GameObject _neweffect = Instantiate(effect, transform.position, transform.rotation);
+
+            spawnedEffects.Add(_neweffect);
+        }
+        // Destroy particles by the amount of lifetime, also destroy this gameobject
+        Invoke("KillObjects", lifeTime);
+    }
+
+    void KillObjects()
+    {
+        foreach(GameObject effect in spawnedEffects)
+        {
+            Destroy(effect);
+        }
+        Destroy(gameObject);
+    }
+
+}
