5f4f0fb407d8e109195f506eaf04679f3c8a58e2
frank078 <46834129+frank078@users.noreply.github.com>
2020-08-17 14:25:38 -0600

    Adding the timer to the game

---
+++ ColorShifter/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float timer;
+    public float timeSpeed = 1f;
+    bool isTimerPlaying = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTimerPlaying)
+        {
+            timer += Time.deltaTime * timeSpeed;
+        } 
+    }
+
+    // example for dicky
+    //if timer = 30
+        //increase difficulty
+}
