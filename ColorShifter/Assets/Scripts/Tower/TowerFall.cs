20a0c8f7204c655f9a114b1b9358d489be8e6d7a
Dicky Chouryan <dchouryan26@gmail.com>
2020-08-18 13:53:20 -0400

    Edit the rb values in TowerFall script
    From pos to negative

--- ColorShifter/Assets/Scripts/Tower/TowerFall.cs
+++ ColorShifter/Assets/Scripts/Tower/TowerFall.cs
@@ -4,14 +4,14 @@ public class TowerFall : MonoBehaviour
 {
     // Variables
     public Rigidbody[] rb;
-    public float fallSpeed = -10f;
+    public float fallSpeed = 10f;
 
     void FixedUpdate()
     {
         for (int i = 0; i < rb.Length; i++)
         {
         // This makes the tower fall towards the player
-        rb[i].velocity = new Vector3(0, fallSpeed, 0);
+        rb[i].velocity = new Vector3(0, -(fallSpeed), 0);
         }
     }
 }
