f924dd5401000b72903ea7efef6fbe325611a6d7
Dicky Chouryan <dchouryan26@gmail.com>
2020-08-26 14:35:53 -0400

    Coins now spins

---
+++ ColorShifter/Assets/Scripts/Obstacles/Coins.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
+    }
+}
