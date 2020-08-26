9064a61b1a3debe042ddaa7ab5e01ce6c4991ff6
frank078 <46834129+frank078@users.noreply.github.com>
2020-08-26 11:40:27 -0600

    Tower turn sound
    Created and imported the tower turning sound

---
+++ ColorShifter/Assets/Scripts/Tower/TowerSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSound : MonoBehaviour
+{
+
+    public AudioSource towerTurn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            towerTurn.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            towerTurn.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            towerTurn.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            towerTurn.Stop();
+        }
+    }
+}
