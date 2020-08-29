86367311aefe2b6a465373f75542411251250dfa
frank078 <46834129+frank078@users.noreply.github.com>
2020-08-29 10:03:27 -0600

    Pause Functionality
    Player can bring the pause menu up

---
+++ ColorShifter/Assets/Scripts/UI/MenuButtons.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void pauseButton()
+    {
+        pauseMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
