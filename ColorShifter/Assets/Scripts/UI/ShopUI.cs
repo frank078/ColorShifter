63b991e5783b1af6458e5e996da134e5b635958e
frank078 <46834129+frank078@users.noreply.github.com>
2020-09-14 17:47:21 -0600

    Adding the shop UI
    Adding the Shop Ui to the other scenes

---
+++ ColorShifter/Assets/Scripts/UI/ShopUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUI : MonoBehaviour
+{
+    // Variables
+    public GameObject mainMenuUI;
+    public GameObject shopUI;
+
+    public void ShopUp()
+    {
+        mainMenuUI.SetActive(false);
+        shopUI.SetActive(true);
+    }
+
+    public void MainMenuUp()
+    {
+        shopUI.SetActive(false);
+        mainMenuUI.SetActive(true);
+    }
+}
