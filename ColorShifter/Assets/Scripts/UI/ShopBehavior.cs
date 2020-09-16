f01a30484ba3cf0d59ad7a05e371affd380b3252
frank078 <46834129+frank078@users.noreply.github.com>
2020-09-16 12:12:28 -0600

    Item scrolling
    Finished the item scrolling in the shop

---
+++ ColorShifter/Assets/Scripts/UI/ShopBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopBehavior : MonoBehaviour
+{
+    // Variables
+    GameObject shopItem;
+    GameObject g;
+    [SerializeField] Transform shopScrollView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shopItem = shopScrollView.GetChild(0).gameObject;
+
+        // change final number depending on how many items are in the shop
+        // remember to also change the constraint count in the content scroll
+        for (int i = 0; i < 5; i++)
+        {
+            g = Instantiate(shopItem, shopScrollView);
+        }
+
+        Destroy(shopItem);
+    }
+}
