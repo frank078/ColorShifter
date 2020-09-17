383ff67414556880dd8d8be6311bccb851a0e7df
Dicky Chouryan <dchouryan26@gmail.com>
2020-09-17 15:56:35 -0400

    Blocker gacha script

--- ColorShifter/Assets/Scripts/Obstacles/CollectiblesManager.cs
+++ ColorShifter/Assets/Scripts/Obstacles/CollectiblesManager.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class CoinsManager : MonoBehaviour
+public class CollectiblesManager : MonoBehaviour
 {
     public GameObject[] coins; // coins in this children
     private int thisTowerNumber;
 
     public GameObject[] diamonds;
+
+    public GameObject[] blockers;
     
 
     private void Start()
     {
         //SUBSCRIPTION
         GameManager.Instance.OnTowerPulling += ModifyCoins;
+        GameManager.Instance.OnTowerPulling += ModifyBlocker;
 
         if(coins.Length == 0)
         {
@@ -29,7 +32,11 @@ public class CoinsManager : MonoBehaviour
 
         //SHUFFLE COINS (CALL ONCE)
         ModifyCoins(99);
+
+        //DISABLE ALL BLOCKERS (CALL ONCE)
+        DisableAllBlocker();
     }
+
 
     void ModifyCoins(int TowerNumber)
     {
@@ -93,6 +100,35 @@ public class CoinsManager : MonoBehaviour
                     }
                 }
             }
+        }
+    }
+
+
+    void ModifyBlocker(int TowerNumber)
+    {
+        if (thisTowerNumber == TowerNumber)
+        {
+            foreach (GameObject _blockers in blockers)
+            {
+                // 15% succession
+                float odds = Random.Range(0f, 1f);
+                if (0.15f >= odds)
+                {
+                    _blockers.SetActive(true);
+                }
+                else
+                {
+                    _blockers.SetActive(false);
+                }
+            }
+        }
+    }
+
+    void DisableAllBlocker()
+    {
+        foreach(GameObject _blockers in blockers)
+        {
+            _blockers.SetActive(false);
         }
     }
 }
