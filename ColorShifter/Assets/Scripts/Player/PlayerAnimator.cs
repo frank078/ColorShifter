2216cfdd297b0c7cbd73990ccddd6baafaacddab
Dicky Chouryan <dchouryan26@gmail.com>
2020-10-02 14:47:56 -0400

    Player animation now moves when player press an input

---
+++ ColorShifter/Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !animator.GetBool("IsRight"))
+        {
+            animator.SetBool("IsRight", true);
+        }
+
+        if(Input.GetKeyUp(KeyCode.RightArrow) && animator.GetBool("IsRight"))
+        {
+            animator.SetBool("IsRight", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && !animator.GetBool("IsLeft") && !animator.GetBool("IsRight"))
+        {
+            animator.SetBool("IsLeft", true);
+            animator.SetBool("IsRight", true);
+        }
+        if(Input.GetKeyUp(KeyCode.LeftArrow) && animator.GetBool("IsLeft") && animator.GetBool("IsRight"))
+        {
+            animator.SetBool("IsLeft", false);
+            animator.SetBool("IsRight", false);
+        }
+    }
+}
